8b271a6177e4418c6d159fa72fb791b8cc3717f4
Sam Daitzman <sam@daitzman.com>
2019-12-26 16:41:07 -0500

    basic api

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log(process.version)
+const group = 1
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/brightness', (req, res) => {
+    res.send('Hello world')
+    console.log(req.query)
+    let newVal = +req.query.brightness
+    console.log(`newVal: ${newVal}`)
+
+    
+
+})
+
+app.listen(port, () => {console.log(`Example app listening on port ${port}!`)})
